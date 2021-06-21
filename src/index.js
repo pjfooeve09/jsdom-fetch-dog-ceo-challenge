1370c4510306c79fa3276a75d053eb40884a6028
Parham Javadi <pjfooeve09@yahoo.com>
2021-06-21 16:14:37 -0700

    commit tree 29a0ccbdd67f018360d190ec73417fe3100bca4a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
+
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchImages();
+    fetchBreeds();
+})
+
+var breedNames = []
+
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    return fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImages(json));
+}
+ 
+
+function renderImages(json) { 
+    const main = document.getElementById('dog-image-container')
+    for (const element of json.message) {
+        const image = document.createElement('img') 
+        image.src = element
+        main.appendChild(image)
+    }
+}
+
+function fetchBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    return fetch(breedUrl)
+      .then(resp => resp.json())
+      .then(json => {
+        for (const key in json.message) {
+            breedNames.push(key);
+        }
+        breedName(breedNames);
+    });
+}
+
+function breedName(breedNames){
+    const breeds = document.getElementById('dog-breeds');
+    for (const breedName of breedNames) {
+        const breedLi = document.createElement('li');
+        breedLi.innerHTML = breedName;
+        breeds.appendChild(breedLi);
+    }
+    changeColor()
+    selectBreeds()
+}
+
+function changeColor(){
+    const listItems = document.getElementsByTagName('li');
+    for (const li of listItems){
+        li.addEventListener('click', function(){
+            li.style.color = 'blue'});
+        }
+}
+
+function selectBreeds (){
+    subBreeds = document.getElementById('breed-dropdown');
+    subBreeds.addEventListener('change', function(){
+        newSubBreedsValue = subBreeds.value;
+        editBreeds(newSubBreedsValue)
+    })
+}
+
+function editBreeds(newSubBreedsValue){
+    emptyList = document.getElementById('dog-breeds')
+    while (emptyList.hasChildNodes()) {  
+        emptyList.removeChild(emptyList.firstChild);
+      }
+    breedSubset = breedNames.filter(breedname => breedname.charAt(0) == newSubBreedsValue)
+    breedName(breedSubset);
+    
+}
